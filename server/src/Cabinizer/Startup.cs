218f955b4b0b715ce077d0e51646e52c1c65ef41
Kristian Hellang <kristian@hellang.com>
2019-10-29 21:09:27 +0100

    Add Google auth to API

--- server/src/Cabinizer/Startup.cs
+++ server/src/Cabinizer/Startup.cs
@@ -1,31 +1,70 @@
+using System.Text.Json;
+using Hellang.Authentication.JwtBearer.Google;
+using Hellang.Middleware.ProblemDetails;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 
 namespace Cabinizer
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        private IConfiguration Configuration { get; }
+
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddProblemDetails();
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(x => x.UseGoogle(
+                    clientId: Configuration["Auth:Google:ClientId"],
+                    hostedDomain: Configuration["Auth:Google:HostedDomain"]));
+
+            services.AddAuthorization(options =>
+            {
+                options.FallbackPolicy = new AuthorizationPolicyBuilder()
+                    .RequireAuthenticatedUser()
+                    .Build();
+            });
+
+            services.AddMvcCore();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            app.UseProblemDetails();
 
             app.UseRouting();
 
+            app.UseAuthentication();
+            app.UseAuthorization();
+
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
+                endpoints.MapGet("/", context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    context.Response.ContentType = "application/json";
+
+                    var writer = new Utf8JsonWriter(context.Response.BodyWriter);
+
+                    writer.WriteStartObject();
+
+                    foreach (var claim in context.User.Claims)
+                    {
+                        writer.WriteString(claim.Type, claim.Value);
+                    }
+
+                    writer.WriteEndObject();
+
+                    return writer.FlushAsync();
                 });
             });
         }
