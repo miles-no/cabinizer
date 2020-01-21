b664f434ad0dff6cdfbe8b54b9789471c0dc01a3
Bjørn Asle Taranger <bjorn.taranger@gmail.com>
2020-01-21 19:59:32 +0100

    Added login via google

--- client-ts/src/app/App.tsx
+++ client-ts/src/app/App.tsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { GoogleLogin } from 'react-google-login';
+
+const responseGoogle = (response) => {
+  console.log(response);
+}
 
 const App: React.FC = () => {
-  return <div>Hello Cabinizer 3000</div>;
+  return <div><h1>Hello Cabinizer 3000</h1>
+    <GoogleLogin
+        clientId="611538057711-dia11nhabvku7cgd0edubeupju1jf4rg.apps.googleusercontent.com"
+        hostedDomain="miles.no"
+        buttonText="Login"
+        autoLoad={true}
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+        cookiePolicy={'single_host_origin'}
+    />
+  </div>;
 };
 
 export default App;
