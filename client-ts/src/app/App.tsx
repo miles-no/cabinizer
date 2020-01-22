45b3931abac8915f0d276646af12802d4458d3a4
Stig Rune <stigrune@gmail.com>
2020-01-22 22:04:22 +0100

    Fixed nonresponsive drawer

--- client-ts/src/app/App.tsx
+++ client-ts/src/app/App.tsx
@@ -1,35 +1,37 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { GoogleLogin } from 'react-google-login';
 import Menu from "./layout/Menu";
 
 
-const loginFailed = (response) => {
+const loginFailed = (response: any) => {
   console.log(response);
 
 }
 
 const App: React.FC = () => {
   const [accessToken, setAccessToken] = useState(null);
-  const [profile, setProfile] = useState( null);
-  const loginSuccess = (response) => {
+  const [profile, setProfile] = useState(null);
+  const loginSuccess = (response: any) => {
     console.log(response);
-setAccessToken(response.access_token);
-setProfile(response.profileObj);
+    setAccessToken(response.access_token);
+    setProfile(response.profileObj);
   }
-
-  return <div>
-    <h1>Hello {profile? profile.name: "Cabinizer 3001"}</h1>
-    {/* <Menu/> */}
+  if (profile) {
+    return (
+      <Menu profile={profile} />
+    );
+  }
+  return (<div>
     <GoogleLogin
-        clientId="611538057711-dia11nhabvku7cgd0edubeupju1jf4rg.apps.googleusercontent.com"
-        hostedDomain="miles.no"
-        buttonText="Login"
-        autoLoad={true}
-        onSuccess={loginSuccess}
-        onFailure={loginFailed}
-        cookiePolicy={'single_host_origin'}
+      clientId="611538057711-dia11nhabvku7cgd0edubeupju1jf4rg.apps.googleusercontent.com"
+      hostedDomain="miles.no"
+      buttonText="Login"
+      autoLoad={true}
+      onSuccess={loginSuccess}
+      onFailure={loginFailed}
+      cookiePolicy={'single_host_origin'}
     />
-  </div>;
+  </div>);
 };
 
 export default App;
--- client-ts/src/app/layout/Menu.tsx
+++ client-ts/src/app/layout/Menu.tsx
@@ -1,65 +1,60 @@
-import React from "react";
-
+import React from 'react';
+import PropTypes from 'prop-types';
+import AppBar from '@material-ui/core/AppBar';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Divider from '@material-ui/core/Divider';
 import Drawer from '@material-ui/core/Drawer';
 import Hidden from '@material-ui/core/Hidden';
+import IconButton from '@material-ui/core/IconButton';
 import InboxIcon from '@material-ui/icons/MoveToInbox';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import MailIcon from '@material-ui/icons/Mail';
-import { makeStyles, useTheme, Theme, createStyles } from '@material-ui/core/styles';
+import MenuIcon from '@material-ui/icons/Menu';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
 
 const drawerWidth = 240;
 
-const useStyles = makeStyles((theme: Theme) =>
-    createStyles({
-        root: {
-            display: 'flex',
-        },
-        drawer: {
-            [theme.breakpoints.up('sm')]: {
-                width: drawerWidth,
-                flexShrink: 0,
-            },
-        },
-        appBar: {
-            [theme.breakpoints.up('sm')]: {
-                width: `calc(100% - ${drawerWidth}px)`,
-                marginLeft: drawerWidth,
-            },
-        },
-        menuButton: {
-            marginRight: theme.spacing(2),
-            [theme.breakpoints.up('sm')]: {
-                display: 'none',
-            },
-        },
-        toolbar: theme.mixins.toolbar,
-        drawerPaper: {
+const useStyles = makeStyles(theme => ({
+    root: {
+        display: 'flex',
+    },
+    drawer: {
+        [theme.breakpoints.up('sm')]: {
             width: drawerWidth,
+            flexShrink: 0,
         },
-        content: {
-            flexGrow: 1,
-            padding: theme.spacing(3),
+    },
+    appBar: {
+        [theme.breakpoints.up('sm')]: {
+            width: `calc(100% - ${drawerWidth}px)`,
+            marginLeft: drawerWidth,
         },
-    }),
-);
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+        [theme.breakpoints.up('sm')]: {
+            display: 'none',
+        },
+    },
+    toolbar: theme.mixins.toolbar,
+    drawerPaper: {
+        width: drawerWidth,
+    },
+    content: {
+        flexGrow: 1,
+        padding: theme.spacing(3),
+    },
+}));
 
-interface ResponsiveDrawerProps {
-    /**
-     * Injected by the documentation to work in an iframe.
-     * You won't need it on your project.
-     */
-    container?: Element;
-}
-
-export default function Menu(props: ResponsiveDrawerProps) {
+function ResponsiveDrawer(props: { container: any; profile: any; }) {
     const { container } = props;
+    const classes = useStyles();
     const theme = useTheme();
-    const classes = useStyles(theme);
     const [mobileOpen, setMobileOpen] = React.useState(false);
 
     const handleDrawerToggle = () => {
@@ -69,6 +64,9 @@ export default function Menu(props: ResponsiveDrawerProps) {
     const drawer = (
         <div>
             <div className={classes.toolbar} />
+            <Typography variant="h6" noWrap>
+                Hello {props.profile.name}
+            </Typography>
             <Divider />
             <List>
                 {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
@@ -93,6 +91,21 @@ export default function Menu(props: ResponsiveDrawerProps) {
     return (
         <div className={classes.root}>
             <CssBaseline />
+            <AppBar position="fixed" className={classes.appBar}>
+                <Toolbar>
+                    <IconButton
+                        color="inherit"
+                        aria-label="open drawer"
+                        edge="start"
+                        onClick={handleDrawerToggle}
+                        className={classes.menuButton}>
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" noWrap>
+                        Cabinizer 3000
+                    </Typography>
+                </Toolbar>
+            </AppBar>
             <nav className={classes.drawer} aria-label="mailbox folders">
                 {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
                 <Hidden smUp implementation="css">
@@ -124,6 +137,42 @@ export default function Menu(props: ResponsiveDrawerProps) {
                     </Drawer>
                 </Hidden>
             </nav>
+            <main className={classes.content}>
+                <div className={classes.toolbar} />
+                <Typography paragraph>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+                    ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
+                    facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
+                    gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
+                    donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
+                    adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras.
+                    Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis
+                    imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
+                    arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
+                    donec massa sapien faucibus et molestie ac.
+        </Typography>
+                <Typography paragraph>
+                    Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper eget nulla
+                    facilisi etiam dignissim diam. Pulvinar elementum integer enim neque volutpat ac
+                    tincidunt. Ornare suspendisse sed nisi lacus sed viverra tellus. Purus sit amet volutpat
+                    consequat mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis risus sed
+                    vulputate odio. Morbi tincidunt ornare massa eget egestas purus viverra accumsan in. In
+                    hendrerit gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem et
+                    tortor. Habitant morbi tristique senectus et. Adipiscing elit duis tristique sollicitudin
+                    nibh sit. Ornare aenean euismod elementum nisi quis eleifend. Commodo viverra maecenas
+                    accumsan lacus vel facilisis. Nulla posuere sollicitudin aliquam ultrices sagittis orci a.
+        </Typography>
+            </main>
         </div>
     );
 }
+
+ResponsiveDrawer.propTypes = {
+    /**
+     * Injected by the documentation to work in an iframe.
+     * You won't need it on your project.
+     */
+    container: PropTypes.instanceOf(typeof Element === 'undefined' ? Object : Element),
+};
+
+export default ResponsiveDrawer;
