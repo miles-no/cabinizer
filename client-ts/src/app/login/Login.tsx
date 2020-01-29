d62fe600937cb3906dd671efe1aca1044e3b5f53
Stig Rune <stigrune@gmail.com>
2020-01-29 20:23:31 +0100

    Added ESLint with prettier

--- client-ts/src/app/login/Login.tsx
+++ client-ts/src/app/login/Login.tsx
@@ -1,30 +1,30 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import { GoogleLogin } from 'react-google-login';
 
 function Login() {
-    const [accessToken, setAccessToken] = useState(null);
-    const [profile, setProfile] = useState(null);
+  const [accessToken, setAccessToken] = useState(null);
+  const [profile, setProfile] = useState(null);
 
-    const loginSuccess = (response: any) => {
-        console.log(response);
-        setAccessToken(response.access_token);
-        setProfile(response.profileObj);
-    }
+  const loginSuccess = (response: any) => {
+    console.log(response);
+    setAccessToken(response.access_token);
+    setProfile(response.profileObj);
+  };
 
-    const loginFailed = (response: any) => {
-        console.log(response);
-    }
+  const loginFailed = (response: any) => {
+    console.log(response);
+  };
 
-    return (
-        <GoogleLogin
-            clientId="611538057711-dia11nhabvku7cgd0edubeupju1jf4rg.apps.googleusercontent.com"
-            hostedDomain="miles.no"
-            buttonText="Login"
-            onSuccess={loginSuccess}
-            onFailure={loginFailed}
-            cookiePolicy={'single_host_origin'}
-        />
-    );
-};
+  return (
+    <GoogleLogin
+      clientId="611538057711-dia11nhabvku7cgd0edubeupju1jf4rg.apps.googleusercontent.com"
+      hostedDomain="miles.no"
+      buttonText="Login"
+      onSuccess={loginSuccess}
+      onFailure={loginFailed}
+      cookiePolicy={'single_host_origin'}
+    />
+  );
+}
 
 export default Login;
